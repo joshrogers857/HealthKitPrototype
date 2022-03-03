1fd28bf372998b9b070835ba1f5254a51ef06cce
joshrogers857 <87278992+joshrogers857@users.noreply.github.com>
2022-03-03 11:11:37 +0000

    Initial Commit

---
+++ HealthKitPrototype/HealthKitPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitPrototypeApp.swift
+//  HealthKitPrototype
+//
+//  Created by Joshua Rogers on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
